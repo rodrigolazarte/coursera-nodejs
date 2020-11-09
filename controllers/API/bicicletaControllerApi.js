4eb33a18b3fe4b023a5f99bd9067973fd46801bd
rodrigolazarte <romalaz94@gmail.com>
2020-11-09 01:42:07 -0300

    agregada integracion con mongodb hacia la api bicicletas (solo falta modificar metodo update)

--- controllers/API/bicicletaControllerApi.js
+++ controllers/API/bicicletaControllerApi.js
@@ -1,3 +1,4 @@
+const { deleteOne } = require('../../models/bicicleta');
 var Bicicleta = require('../../models/bicicleta');
 
 exports.bicicleta_list = function(req, res){
@@ -9,7 +10,8 @@ exports.bicicleta_list = function(req, res){
 }
 
 exports.bicicleta_create = function(req, res){
-    var bici = new Bicicleta({code: req.body.code, color: req.body.color, modelo: req.body.modelo, ubicacion: [req.body.lat, req.body.lng] });
+    var bici = new Bicicleta({code: req.body.code, color: req.body.color, modelo: req.body.modelo, ubicacion: [req.body.lat, req.body.lng]});
+    //Bicicleta.add(bici);
 
     bici.save(function(err){
         res.status(200).json({
@@ -19,8 +21,11 @@ exports.bicicleta_create = function(req, res){
 }
 
 exports.bicicleta_delete = function(req, res){
-    Bicicleta.removeById(req.body.id);
-    res.status(204).send();
+    Bicicleta.findOne({code: req.body.code}, function(err, bici){
+        bici.deleteOne(function(err){
+            res.status(204).send();
+        });
+    })
 }
 
 exports.bicicleta_update = function(req, res){
